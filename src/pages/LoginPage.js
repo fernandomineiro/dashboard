c228a9240a83f632b69db13920d9cf2cf83499c1
Fernando Fernandes <fernandofitilan@hotmail.com>
2023-04-27 12:29:16 -0300

    up

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,7 @@
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
+
 // @mui
 import { styled } from '@mui/material/styles';
 import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
@@ -9,6 +12,10 @@ import Logo from '../components/logo';
 import Iconify from '../components/iconify';
 // sections
 import { LoginForm } from '../sections/auth/login';
+import AuthService from "../services/auth.service";
+
+
+
 
 // ----------------------------------------------------------------------
 
@@ -38,10 +45,22 @@ const StyledContent = styled('div')(({ theme }) => ({
   padding: theme.spacing(12, 0),
 }));
 
+// const required = (value) => {
+//   if (!value) {
+//     return (
+//       <div className="invalid-feedback d-block">
+//         This field is required!
+//       </div>
+//     );
+//   }
+// };
+
 // ----------------------------------------------------------------------
 
 export default function LoginPage() {
   const mdUp = useResponsive('up', 'md');
+ 
+ 
 
   return (
     <>
@@ -73,30 +92,7 @@ export default function LoginPage() {
              Logar no sistema
             </Typography>
 
-            <Typography variant="body2" sx={{ mb: 5 }}>
-              Você não tem conta? {''}
-              <Link variant="subtitle2">Vamos começar</Link>
-            </Typography>
-
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OU
-              </Typography>
-            </Divider>
+            
 
             <LoginForm />
           </StyledContent>
--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,31 +1,286 @@
-import { useState } from 'react';
+import React, { useState, useRef } from "react";
 import { useNavigate } from 'react-router-dom';
 // @mui
-import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { Link, Stack, IconButton, Typography, InputAdornment, TextField, Checkbox } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 // components
+import Form from "react-validation/build/form";
+import isEmail from 'validator/lib/isEmail';
+
 import Iconify from '../../../components/iconify';
-
+import AuthService from "../../../services/auth.service";
 // ----------------------------------------------------------------------
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="invalid-feedback d-block">
+        Campo obrigatório!
+      </div>
+    );
+  }
+  return false
+};
 
+const validEmail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="invalid-feedback d-block">
+        This is not a valid email.
+      </div>
+    );
+  }
+  return false
+};
+
+const vusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="invalid-feedback d-block">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+  return false
+};
+
+const vpassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="invalid-feedback d-block">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+  return false
+};
 export default function LoginForm() {
   const navigate = useNavigate();
 
-  const [showPassword, setShowPassword] = useState(false);
+  const form = useRef();
+  const checkBtn = useRef();
 
-  const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [page, setPage] = useState(true);
+  const [successful, setSuccessful] = useState(false);
+  const [email, setEmail] = useState("");
+ 
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
   };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    setMessage("");
+    setLoading(true);
+
+    form.current.validateAll();
+    console.log(checkBtn)
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.login(username, password).then(
+        () => {
+          navigate("/profile");
+          window.location.reload();
+        },
+        (error) => {
+          console.log(error)
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          setLoading(false);
+          setMessage(resMessage);
+        }
+      );
+    } else {
+      setLoading(false);
+    }
+  };
+
+  const changeMode = () =>{
+    setPage(!page);
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    setMessage("");
+    setSuccessful(false);
+
+    form.current.validateAll();
+
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.register(username, email, password).then(
+        (response) => {
+          setMessage(response.data.message);
+          setSuccessful(true);
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          setMessage(resMessage);
+          setSuccessful(false);
+        }
+      );
+    }
+  };
+
+  
+
 
   return (
     <>
+    <Typography variant="body2" sx={{ mb: 5 }}>
+              Você não tem conta? {''}
+              <Link variant="subtitle2" onClick={()=>changeMode()}>Vamos começar</Link>
+            </Typography>
+    {page ? 
+     <Form onSubmit={handleLogin} ref={form}>
+          <div className="form-group">
+            <div htmlFor="username">Username</div>
+            <Input
+              type="text"
+              className="form-control"
+              name="username"
+              value={username}
+              onChange={onChangeUsername}
+              validations={[required]}
+            />
+           
+          </div>
+
+          <div className="form-group">
+            <div htmlFor="password">Password</div>
+            <Input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+              validations={[required]}
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary btn-block" disabled={loading}>
+              
+              <span>Login</span>
+            </button>
+          </div>
+
+          {message && (
+            <div className="form-group">
+              <div className="alert alert-danger" role="alert">
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: "none" }} ref={checkBtn} />
+        </Form>
+        : 
+        
+        <Form onSubmit={handleRegister} ref={form}>
+          {!successful && (
+            <div>
+              <div className="form-group">
+                <div>Username</div>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={onChangeUsername}
+                  validations={[required, vusername]}
+                />
+              </div>
+
+              <div className="form-group">
+                <div>Email</div>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
+
+              <div className="form-group">
+                <div>Password</div>
+                <Input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value={password}
+                  onChange={onChangePassword}
+                  validations={[required, vpassword]}
+                />
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-primary btn-block">Sign Up</button>
+              </div>
+            </div>
+          )}
+
+          {message && (
+            <div className="form-group">
+              <div
+                className={
+                  successful ? "alert alert-success" : "alert alert-danger"
+                }
+                role="alert"
+              >
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: "none" }} ref={checkBtn} />
+        </Form>
+        }
+    {/* <Form onSubmit={handleLogin} ref={form}>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField  type="text"
+              className="form-control"
+              name="username"
+              value={username}
+              onChange={onChangeUsername}
+              validations={[required]} />
 
         <TextField
-          name="password"
-          label="Password"
-          type={showPassword ? 'text' : 'password'}
+         className="form-control"
+         name="password"
+         value={password}
+         onChange={onChangePassword}
+         validations={[required]}
+         type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -37,17 +292,26 @@ export default function LoginForm() {
           }}
         />
       </Stack>
-
+      
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
         <Checkbox name="remember" label="Remember me" />- Me Manter conectado
         <Link variant="subtitle2" underline="hover">
           Perdeu senha?
         </Link>
       </Stack>
+      
+      {message && (
+            <div className="form-group">
+              <div className="alert alert-danger" role="alert">
+                {message}
+              </div>
+            </div>
+          )}
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" ref={checkBtn}>
         Logar
       </LoadingButton>
+      </Form> */}
     </>
   );
 }
